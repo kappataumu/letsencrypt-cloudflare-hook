2528535d8a0eeb2ee30198ec87c04ac38c1a40d2
Matthew Gyurgyik <matthew@pyther.net>
2016-04-02 11:15:29 -0400

    add unchanged_cert hook

--- hook.py
+++ hook.py
@@ -139,12 +139,15 @@ def deploy_cert(args):
     logger.info(' + ssl_certificate_key: {0}'.format(privkey_pem))
     return
 
+def unchanged_cert(args):
+    return
 
 def main(argv):
     ops = {
         'deploy_challenge': create_txt_record,
         'clean_challenge' : delete_txt_record,
         'deploy_cert'     : deploy_cert,
+        'unchanged_cert'  : unchanged_cert,
     }
     logger.info(" + CloudFlare hook executing: {0}".format(argv[0]))
     ops[argv[0]](argv[1:])
